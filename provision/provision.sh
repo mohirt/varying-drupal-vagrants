9a5e6400399ea71c1c64687a16fc229e1fe1d429
Joseph White <dignan89@gmail.com>
2014-01-10 10:19:02 -0800

    initial commit: removed most all wordpress related items and added in drush install and configuration, drupal stable install and configuration, nginx virtual site configuration for initial drupal install

--- provision/provision.sh
+++ provision/provision.sh
@@ -70,7 +70,7 @@ apt_package_check_list=(
 
 	# other packages that come in handy
 	imagemagick
-	subversion
+	# subversion
 	git-core
 	zip
 	unzip
@@ -357,20 +357,19 @@ else
 fi
 
 if [[ $ping_result == *bytes?from* ]]; then
-	# WP-CLI Install
-	if [[ ! -d /srv/www/wp-cli ]]; then
-		echo -e "\nDownloading wp-cli, see http://wp-cli.org"
-		git clone git://github.com/wp-cli/wp-cli.git /srv/www/wp-cli
-		cd /srv/www/wp-cli
-		composer install
+
+	## Drush install
+	if[[ ! -d /srv/www/drush ]]; then
+		echo -e "\nDownloading drush..."
+		git clone https://github.com/drush-ops/drush.git /srv/www/drush-ops
 	else
-		echo -e "\nUpdating wp-cli..."
-		cd /srv/www/wp-cli
+		echo -e "\nUpdating drush..."
+		cd /srv/www/drush
 		git pull --rebase origin master
-		composer update
 	fi
-	# Link `wp` to the `/usr/local/bin` directory
-	ln -sf /srv/www/wp-cli/bin/wp /usr/local/bin/wp
+	# Link 'drush' to the '/usr/local/bin' directory
+		ln -sf /srv/www/drush/drush /usr/local/bin/drush
+
 
 	# Download and extract phpMemcachedAdmin to provide a dashboard view and
 	# admin interface to the goings on of memcached when running
@@ -406,82 +405,15 @@ if [[ $ping_result == *bytes?from* ]]; then
 		git pull --rebase origin master
 	fi
 
-	# Sniffs WordPress Coding Standards
-	if [[ ! -d /srv/www/phpcs/CodeSniffer/Standards/WordPress ]]; then
-		echo -e "\nDownloading WordPress-Coding-Standards, snifs for PHP_CodeSniffer, see https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards"
-		git clone git://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git /srv/www/phpcs/CodeSniffer/Standards/WordPress
-	else
-		echo -e "\nUpdating PHP_CodeSniffer..."
-		cd /srv/www/phpcs/CodeSniffer/Standards/WordPress
-		git pull --rebase origin master
-	fi
-
-	# Install and configure the latest stable version of WordPress
-	if [[ ! -d /srv/www/wordpress-default ]]; then
-		echo "Downloading WordPress Stable, see http://wordpress.org/"
-		cd /srv/www/
-		curl -O http://wordpress.org/latest.tar.gz
-		tar -xvf latest.tar.gz
-		mv wordpress wordpress-default
-		rm latest.tar.gz
-		cd /srv/www/wordpress-default
-		echo "Configuring WordPress Stable..."
-		wp core config --dbname=wordpress_default --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
-define( 'WP_DEBUG', true );
-PHP
-		wp core install --url=local.wordpress.dev --quiet --title="Local WordPress Dev" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"
-	else
-		echo "Updating WordPress Stable..."
-		cd /srv/www/wordpress-default
-		wp core upgrade
-	fi
-
-	# Checkout, install and configure WordPress trunk via core.svn
-	if [[ ! -d /srv/www/wordpress-trunk ]]; then
-		echo "Checking out WordPress trunk from core.svn, see http://core.svn.wordpress.org/trunk"
-		svn checkout http://core.svn.wordpress.org/trunk/ /srv/www/wordpress-trunk
-		cd /srv/www/wordpress-trunk
-		echo "Configuring WordPress trunk..."
-		wp core config --dbname=wordpress_trunk --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
-define( 'WP_DEBUG', true );
-PHP
-		wp core install --url=local.wordpress-trunk.dev --quiet --title="Local WordPress Trunk Dev" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"
-	else
-		echo "Updating WordPress trunk..."
-		cd /srv/www/wordpress-trunk
-		svn up --ignore-externals
-	fi
-
-	# Checkout, install and configure WordPress trunk via develop.svn
-	if [[ ! -d /srv/www/wordpress-develop ]]; then
-		echo "Checking out WordPress trunk from develop.svn, see http://develop.svn.wordpress.org/trunk"
-		svn checkout http://develop.svn.wordpress.org/trunk/ /srv/www/wordpress-develop
-		cd /srv/www/wordpress-develop/src/
-		echo "Configuring WordPress develop..."
-		wp core config --dbname=wordpress_develop --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
-// Allow (src|build).wordpress-develop.dev to share the same database
-if ( 'build' == basename( dirname( __FILE__) ) ) {
-	define( 'WP_HOME', 'http://build.wordpress-develop.dev' );
-	define( 'WP_SITEURL', 'http://build.wordpress-develop.dev' );
-}
-
-define( 'WP_DEBUG', true );
-PHP
-		wp core install --url=src.wordpress-develop.dev --quiet --title="WordPress Develop" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"
-		cp /srv/config/wordpress-config/wp-tests-config.php /srv/www/wordpress-develop/
-		cd /srv/www/wordpress-develop/
-		npm install &>/dev/null
-	else
-		echo "Updating WordPress develop..."
-		cd /srv/www/wordpress-develop/
-		svn up
-		npm install &>/dev/null
-	fi
-
-	if [[ ! -d /srv/www/wordpress-develop/build ]]; then
-		echo "Initializing grunt in WordPress develop... This may take a few moments."
-		cd /srv/www/wordpress-develop/
-		grunt
+	# Install and configure the latest stable version of Drupal
+	if [[ ! -d /srv/www/drupal-stable ]]; then
+		echo "Downloading Drupal Stable, see http://drupal.org"
+		cd /srv/www
+		drush dl drupal
+		mv drupal* drupal-stable
+		cd /srv/www/drupal-stable/sites
+		echo "Configuring Drupal Stable..."
+		drush si --db-url=mysql://root:root@localhost/drupal_test --account-name=admin --account-pass=password --db-su=root --db-su-pw=root --site-name='Welcome to My Drupal Site' --y
 	fi
 
 	# Download phpMyAdmin
--- www/default/index.php
+++ www/default/index.php
@@ -14,13 +14,13 @@ if ( file_exists( 'dashboard-custom.php' ) ) {
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Varying Vagrant Vagrants Dashboard</title>
+	<title>Varying Drupal Vagrants Dashboard</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 <ul class="nav">
 	<li class="active"><a href="#">Home</a></li>
-	<li><a href="http://github.com/10up/varying-vagrant-vagrants">Repository</a></li>
+	<li><a href="http://github.com/gman29/varying-drupal-vagrants">Repository</a></li>
 	<li><a href="database-admin/">phpMyAdmin</a></li>
 	<li><a href="memcached-admin/">phpMemcachedAdmin</a></li>
 	<li><a href="webgrind/">Webgrind</a></li>
@@ -28,10 +28,7 @@ if ( file_exists( 'dashboard-custom.php' ) ) {
 </ul>
 
 <ul class="nav">
-	<li><a href="http://local.wordpress.dev/">http://local.wordpress.dev</a> for WordPress stable</li>
-	<li><a href="http://local.wordpress-trunk.dev/">http://local.wordpress-trunk.dev</a> for WordPress trunk</li>
-	<li><a href="http://src.wordpress-develop.dev/">http://src.wordpress-develop.dev</a> for trunk WordPress development files</li>
-	<li><a href="http://build.wordpress-develop.dev/">http://build.wordpress-develop.dev</a> for a Grunt build of those development files</li>
+	<li><a href="http://local.drupal.dev/">http://local.drupal.dev</a> for Drupal stable</li>
 </ul>
 </body>
 </html>
